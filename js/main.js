7014ef6254b9a1f02ad0e53afac13182efc13bb4
jeickson.oliveira <jeickson.oliveira@advise.com.br>
2023-08-21 20:40:04 -0300

    Atualizando codigos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,55 @@
 jQuery(document).ready(function ($) {
-
   // Stick the header at top on scroll
-  $("#header").sticky({
+  $('#header').sticky({
     topSpacing: 0,
-    zIndex: '50'
+    zIndex: '50',
   });
 
   // Intro background carousel
-  $("#intro-carousel").owlCarousel({
+  $('#intro-carousel').owlCarousel({
     autoplay: true,
     dots: false,
     loop: true,
     animateOut: 'fadeOut',
-    items: 1
+    items: 1,
+  });
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+    },
   });
 
   // Initiate the wowjs animation library
   new WOW().init();
-
-
 });
